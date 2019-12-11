73b58a1177434f9c1e7c51788a181a6c43e84120
jasenfinch <jsf9@aber.ac.uk>
2019-12-11 01:34:54 +0000

    mds() now exported

--- R/rf.R
+++ R/rf.R
@@ -13,26 +13,3 @@ rf <- function(analysisTable, cls, nreps = 100, seed = 1234){
     randomForest(analysisTable,cls,proximity = T)
   })
 }
-
-#' @importFrom tibble as_tibble
-#' @importFrom stats cmdscale
-
-mds <- function(rfModels){
-  rfModels %>%
-    map(~{.$proximity %>%
-        as_tibble() %>%
-        rowid_to_column(var = 'Sample1') %>%
-        gather('Sample2','Proximity',-Sample1)}) %>%
-    bind_rows(.id = 'Iteration') %>%
-    mutate(Sample2 = as.numeric(Sample2)) %>%
-    group_by(Sample1,Sample2) %>%
-    summarise(Proximity = mean(Proximity)) %>%
-    spread(Sample2,Proximity) %>%
-    tbl_df() %>%
-    select(-Sample1) %>%
-    as.matrix() %>%
-    {1 - .} %>%
-    cmdscale() %>%
-    as_tibble() %>%
-    rename(`Dimension 1` = V1,`Dimension 2` = V2)
-}
