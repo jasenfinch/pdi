76408853a9727e9768430945a7b28960c50b78c4
jasenfinch <jsf9@aber.ac.uk>
2020-07-20 02:23:11 +0100

    PDI and DAI computation selection now available in calcDIs

--- R/calcDIs.R
+++ R/calcDIs.R
@@ -1,12 +1,11 @@
 #' Calculate Decline Indexes
-#' @description Calculate Phenotypic Decline Index (PDI) and Decline Acuteness Index (DAI)
-#' @param rfModels list containing random forest models
-#' @param invertPDI invert the PDI scale? TRUE/FALSE
-#' @param invertDAI invert the DAI scale? TRUE/FALSE
-#' @importFrom magrittr %>%
+#' @description Calculate Phenotypic Decline Index (PDI) and Decline Acuteness Index (DAI).
+#' @param rfModels list containing random forest models as returned by \code{rf()}
+#' @param invertPDI invert the PDI scale? TRUE/FALSE. Ignored if argument PDI is FALSE
+#' @param invertDAI invert the DAI scale? TRUE/FALSE. Ignored if argument DAI is FALSE
 #' @export
 
-calcDIs <- function(rfModels, invertPDI = TRUE, invertDAI = TRUE){
+calcDIs <- function(rfModels, PDI = TRUE, DAI = TRUE, invertPDI = TRUE, invertDAI = TRUE){
   DIs <- rfModels %>%
     mds() %>%
     rename(PDI = `Dimension 1`,DAI = `Dimension 2`) %>%
@@ -14,16 +13,26 @@ calcDIs <- function(rfModels, invertPDI = TRUE, invertDAI = TRUE){
            DAI = minMaxScale(DAI) %>%
              {2 * . - 1})
   
-  if (isTRUE(invertPDI)) {
+  if (isFALSE(PDI)) {
     DIs <- DIs %>%
-      mutate(PDI = PDI %>%
-               {(. - 1) * -1})
+      select(-PDI)
+  } else {
+    if (isTRUE(invertPDI)) {
+      DIs <- DIs %>%
+        mutate(PDI = PDI %>%
+                 {(. - 1) * -1})
+    }
   }
   
-  if (isTRUE(invertDAI)) {
+  if (isFALSE(DAI)) {
     DIs <- DIs %>%
-      mutate(DAI = DAI %>%
-               {. * -1})
+      select(-DAI)
+  } else {
+    if (isTRUE(invertDAI)) {
+      DIs <- DIs %>%
+        mutate(DAI = DAI %>%
+                 {. * -1})
+    }
   }
   
   return(DIs)
