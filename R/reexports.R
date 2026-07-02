37ea0559e11dd5f42371b9d22a2a52db9916c417
jasenfinch <3043780+jasenfinch@users.noreply.github.com>
2026-07-02 11:48:31 +0100

    docs: add header for re-export functions

--- R/reexports.R
+++ R/reexports.R
@@ -1,11 +1,16 @@
+#' Re-exported functions
+#'
+#' These functions are re-exported from other packages.
+#'
+#' @name reexports
+#' @keywords internal
 #' @importFrom magrittr %>%
 #' @rdname reexports
 #' @export
-
 magrittr::`%>%`
 
 #' @importFrom purrr map
 #' @rdname reexports
 #' @export
-
 purrr::map
+
